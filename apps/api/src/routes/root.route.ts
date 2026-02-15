7058e06905b617ad19e8b3b0ab4cacad4dab8d27
ngogiahuandev <ngogiahuan23122003@gmail.com>
2026-02-16 03:57:34 +0700

    remove elysiajs

--- apps/api/src/routes/root.route.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from 'elysia';
-import { env } from '../config/env';
-
-export const rootRoute = new Elysia({ prefix: '' }).get('/', () => ({
-  message: 'Analy API 2',
-  version: '1.0.0',
-  environment: env.NODE_ENV,
-  timestamp: new Date().toISOString(),
-}));
